6fdfcaacc27baf40f18eece94b770cd3bd4e6ed3
Avanish Kumar Gupta <91612116+Avanish-Gupta-CSE@users.noreply.github.com>
2023-06-18 20:22:54 +0530

    update

---
+++ my-app/src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { HabitProvider } from "./context/HabitContext";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <HabitProvider>
+        <App />
+      </HabitProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
